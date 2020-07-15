5251b81f6b833298e6ab14acdc94afcf704b167d
Alessandro  (Notebook) <alex.ixs@hotmail.com>
2020-07-15 16:14:50 +0200

    Layout ready

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <meta charset="utf-8">
+    <title>Google Faq</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="wrapper">
+      <!-- Begin header -->
+      <header>
+        <div class="logo">
+          <img src="img/g-logo.png" alt="logo">
+          <h3>Privacy e Termini</h3>
+        </div>
+        <nav>
+          <ul>
+            <li>Introduzione</li>
+            <li>Norme sulla privacy</li>
+            <li>Termini di servizio</li>
+            <li>Tecnologie</li>
+            <li class="active">Domande frequenti</li>
+          </ul>
+
+          <div class="account">
+            <img src="img/user.jpg" alt="user">
+            <span>yourname@gmail.com</span>
+          </div>
+        </nav>
+      </header>
+      <!-- End header -->
+
+      <!-- Begin main -->
+      <main>
+        <div class="container_1170">
+
+        </div>
+      </main>
+      <!-- End main -->
+
+    </div>
+
+  </body>
+</html>
